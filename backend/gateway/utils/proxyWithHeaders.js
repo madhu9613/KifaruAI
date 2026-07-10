3949dd54c1acba765253313407debeffe2f2c725
mady9613 <rajkhowamadhujya0@gmail.com>
2026-07-11 03:32:44 +0530

    starting microservice API gateway, enable bufferresponse fall such that we can use sse for streaming token fro llm responses and diff auth service proxy

---
+++ backend/gateway/utils/proxyWithHeaders.js
@@ -0,0 +1,15 @@
+import proxy from "express-http-proxy";
+
+export const proxyWithUser = (serviceUrl, proxyOptions = {}) => {
+  return proxy(serviceUrl, {
+    ...proxyOptions, // allows overriding bufferResponse, etc.
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      if (srcReq.user) {
+        proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;
+        proxyReqOpts.headers["x-user-email"] = srcReq.user.email;
+        proxyReqOpts.headers["x-user-avatar"] = srcReq.user.avatar;
+      }
+      return proxyReqOpts;
+    }
+  });
+};

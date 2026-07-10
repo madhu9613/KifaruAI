e1fc74f6605f725b2afb491fc1e04dc53f9f129d
mady9613 <rajkhowamadhujya0@gmail.com>
2026-07-11 05:16:49 +0530

    fix in proxy for payload size

--- backend/gateway/utils/proxyWithHeaders.js
+++ backend/gateway/utils/proxyWithHeaders.js
@@ -3,6 +3,7 @@ import proxy from "express-http-proxy";
 export const proxyWithUser = (serviceUrl, proxyOptions = {}) => {
   return proxy(serviceUrl, {
     ...proxyOptions, // allows overriding bufferResponse, etc.
+    limit: proxyOptions.limit || "50mb", 
     proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
       if (srcReq.user) {
         proxyReqOpts.headers["x-user-id"] = srcReq.user.userId;

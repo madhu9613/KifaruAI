abe7f834e5d0d4e430abd238a29be19d199613e7
mady9613 <rajkhowamadhujya0@gmail.com>
2026-07-11 15:18:07 +0530

    shared radis correction for bullmq connection

--- backend/shared/redis/redis.js
+++ backend/shared/redis/redis.js
@@ -3,13 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const redis = new Redis(process.env.REDIS_URL);
+console.log(process.env.REDIS_URL);
+
+const redis = new Redis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null,
+});
 
 redis.on("connect", () => {
   console.log("✅ Redis Connected");
 });
 
-
-
+redis.on("error", (err) => {
+  console.error(err);
+});
 
 export default redis;

4e9092b21460dda54676987974f0419ccdd11c1e
mady9613 <rajkhowamadhujya0@gmail.com>
2026-07-15 17:21:09 +0530

    fix firebase config

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,6 +1,13 @@
 import { initializeApp, cert } from "firebase-admin/app";
 
-import serviceAccount from "../serviceAccount.json" with { type: "json" };
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+};
 
 export const app = initializeApp({
   credential: cert(serviceAccount),

0d9c284902d8808ea34db1ec6e934004e6b630e1
mady9613 <rajkhowamadhujya0@gmail.com>
2026-07-15 22:39:56 +0530

    fix cors issue for production

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -12,14 +12,22 @@ import cookieParser from "cookie-parser"
 dotenv.config();
 const app = express();
 const port=process.env.PORT || 5000
+
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173"
+];
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+  origin: allowedOrigins,
+  credentials: true
 }));
+
 app.use(
   "/uploads",
   express.static("uploads")
-);
+) 
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cookieParser());
